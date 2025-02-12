317fdf448031509e0aea5486c7fa32b7a9d3502c
Renato <renaato.d.m@gmail.com>
2025-02-12 18:12:15 -0300

    feature(kafka-spring-lib): initial configuration

--- payment-api/src/main/kotlin/com/payment/paymentflowchallenge/dataprovider/queue/kafka/KafkaQueueProducer.kt
+++ payment-api/src/main/kotlin/com/payment/paymentflowchallenge/dataprovider/queue/kafka/KafkaQueueProducer.kt
@@ -1,16 +1,13 @@
 package com.payment.paymentflowchallenge.dataprovider.queue.kafka
 
 import com.payment.paymentflowchallenge.core.common.util.toJson
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.clients.producer.RecordMetadata
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Service
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
+import java.util.concurrent.CompletableFuture
+
 
 @Service
 class KafkaQueueProducer(
@@ -18,23 +15,21 @@ class KafkaQueueProducer(
 ) {
     private final val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
-    suspend fun <K, V> KafkaProducer<K, V>.asyncSend(record: ProducerRecord<K, V>) =
-        suspendCoroutine<RecordMetadata> { continuation ->
-            send(record) { metadata, exception ->
-                exception?.let(continuation::resumeWithException)
-                    ?: continuation.resume(metadata)
-            }
-        }
-
     fun send(topicName: String, messageValue: Any) {
         val jsonMessage = messageValue.toJson()
 
-        kafkaTemplate.send(topicName, jsonMessage)
-        producer.send(ProducerRecord(topicName, jsonMessage)) { metadata, exception ->
+        val future: CompletableFuture<SendResult<String, String>> =
+            kafkaTemplate.send(topicName, jsonMessage)
+
+        future.whenComplete { result, exception ->
             if (exception != null) {
-                log.info("Error sending message: ${exception.message}")
+                log.error("Error sending message: $jsonMessage", exception)
             } else {
-                log.info("Message sent successfully to topic ${metadata.topic()} on partition ${metadata.partition()} with offset ${metadata.offset()}")
+                log.info(
+                    "Message sent successfully to topic ${result.recordMetadata.topic()} " +
+                        "on partition ${result.recordMetadata.partition()} " +
+                        "with offset ${result.recordMetadata.offset()}"
+                )
             }
         }
     }
